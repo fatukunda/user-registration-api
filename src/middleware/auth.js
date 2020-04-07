6a4fc55ede5ea5f1507f27d2dde1fa110ce1814f
Paul Petrov <paul@bargamut.ru>
2020-04-07 03:12:08 +0300

    Fix indents

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,17 +2,17 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
 const auth = async(req, res, next) => {
-		const token = req.header('Authorization').replace('Bearer ', '')
-		
+    const token = req.header('Authorization').replace('Bearer ', '')
+
     try {
-				const data = jwt.verify(token, process.env.JWT_KEY)
+        const data = jwt.verify(token, process.env.JWT_KEY)
         const user = await User.findOne({ _id: data._id, 'tokens.token': token })
 
-				if (!user) {
+        if (!user) {
             throw new Error()
         }
 
-				req.user = user
+        req.user = user
         req.token = token
         next()
     } catch (error) {
