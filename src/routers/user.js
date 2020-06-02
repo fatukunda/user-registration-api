19a550fe5c4680a1fe366e0c5a329ec4588c0239
Vu Dang <>
2020-06-02 15:22:58 +0700

    Fix: findByCredentials -> throw new Error() with correct ErrorConstructor

--- src/routers/user.js
+++ src/routers/user.js
@@ -27,7 +27,7 @@ router.post('/users/login', async(req, res) => {
         const token = await user.generateAuthToken()
         res.send({ user, token })
     } catch (error) {
-        res.status(400).send(error)
+        res.status(400).send({ error: error.message })
     }
 
 })
